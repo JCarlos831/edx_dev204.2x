4e933ef5efb70a80da0f1d78af448ff45bf02239
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-05-17 16:04:21 -0400

    Coding along with "Creating a Class Demo" video

---
+++ edxoop/edxoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace edxoop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine._location = "Kitchen";
+            myMachine._model = "DM1000";
+
+            // Write location to console
+            Console.WriteLine(myMachine._location);
+            // Run MakeCappuccino function
+            myMachine.MakeCappuccino();
+        }
+    }
+}
