b64296aedbecfe65551d3ed5271b885074fc9676
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-05-17 16:01:15 -0400

    Coding along with "Creating a Class Demo" video

---
+++ edxoop/edxoop/DrinksMachine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace edxoop
+{
+    public class DrinksMachine
+    {
+        // The following statements declare private member variables
+        public string _location { get; set; }
+        public string _make { get; set; }
+        public string _model { get; set; }
+
+        // The following statements declare public methods
+        public void MakeCappuccino()
+        {
+            // Method logic goes here
+            Console.WriteLine("Cappuccino is made.");
+        }
+
+        public void MakeEspresso()
+        {
+            // Method logic goes here
+        }
+    }
+}
