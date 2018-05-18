22052ec219e392bd55215d44d225489cd26d2922
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-05-18 15:02:50 -0400

    Static Class Demo

--- edxoop/edxoop/Program.cs
+++ edxoop/edxoop/Program.cs
@@ -6,15 +6,7 @@ namespace edxoop
     {
         static void Main(string[] args)
         {
-            DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
-            // myMachine.Location = "Kitchen";
-            // myMachine.Model = "DM1000";
-
-            Console.WriteLine(myMachine.Location);
-            Console.WriteLine(myMachine.Make);
-            Console.WriteLine(myMachine.Model);
-
-            myMachine.MakeCappuccino();
+            Console.WriteLine(Math.Pow(2, 8));
         }
     }
 }
