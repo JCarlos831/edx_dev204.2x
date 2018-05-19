0c66e88c2359989c7a5d0c0185034bd9d700ce31
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-05-19 11:23:49 -0400

    Module 1 Tutorial Labs done. Getting started on Self Assessment Lab.

---
+++ mod1_self_assessment_lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mod1_self_assessment_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ mod1_self_assessment_lab/Teacher.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace mod1_self_assessment_lab
+{
+  
+}
